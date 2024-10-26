b5a2c384068d48e249f6578fc52f49993ca79079
John Johnston <58345351+leatherman55@users.noreply.github.com>
2024-10-26 18:52:53 -0500

    Add files via upload
    context encoding added

---
+++ train.py
@@ -0,0 +1,55 @@
+
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset, random_split
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from transformer_model import TransformerModel
+from config import *
+import torch.nn.functional as F
+
+# Sample Training Module
+
+class TransformerTrainer(pl.LightningModule):
+    def __init__(self):
+        super(TransformerTrainer, self).__init__()
+        self.model = TransformerModel(input_dim, d_model, N, heads, d_ff, output_dim)
+        self.learning_rate = learning_rate
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log('val_loss', loss, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        return optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+# Prepare training and validation data
+def prepare_data():
+    # Sample data for demonstration purposes
+    x_data = torch.rand((100, input_dim))
+    y_data = torch.rand((100, output_dim))
+    dataset = TensorDataset(x_data, y_data)
+    
+    # Split data into training and validation sets
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size)
+    
+    return train_loader, val_loader
